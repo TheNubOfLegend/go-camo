35d28e05f74f05fb3f8c8396b6022df90d4aee91
thenuboflegend <thenuboflegend@proton.me>
2024-10-01 10:02:52 -0500

    inc offset after report

--- cmd/main.go
+++ cmd/main.go
@@ -22,23 +22,70 @@ func main() {
   language.translation["lsa"] = "if"
 
   for orig, trans := range language.translation {
-    for i := range file {
-      if i + len(orig) < len(file) && orig[0] == file[i] {
-        match := true
-        for j := range len(orig) - 1 {
-          if orig[j] != file[i+j] {
-            match = false
-          }
-        }
-        if match {
-          for _, char := range language.whitespace {
-            if file[i+len(orig)] == char {
-              file = append(file[:i], append([]byte(trans), file[i+len(orig):]...)...)
-            }
-          }
-        }
-      }
+    occurrences := horspool(file, []byte(orig))
+    for _, val := range occurrences {
+      file = append(file[:val], append([]byte(trans), file[val+len(orig):]...)...)
     }
+
+    // for i := range file {
+    //   if i + len(orig) < len(file) && orig[0] == file[i] {
+    //     match := true
+    //     for j := range len(orig) - 1 {
+    //       if orig[j] != file[i+j] {
+    //         match = false
+    //       }
+    //     }
+    //     if match {
+    //       for _, char := range language.whitespace {
+    //         if file[i+len(orig)] == char {
+    //           file = append(file[:i], append([]byte(trans), file[i+len(orig):]...)...)
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
   }
   fmt.Println(string(file))
+}
+
+func max(x, y int) int {
+    if x > y {
+        return x
+    }
+    return y
+}
+
+func horspool(file, pattern []byte) []int {
+    // file, err := os.ReadFile("test")
+    // if err != nil {
+    //     fmt.Println("no file nerd")
+    // }
+
+    // var pattern []byte = []byte("good")
+    var result []int
+    var badChar [256]int
+
+    for i := range pattern {
+        badChar[pattern[i]] = i
+    }
+
+    offset := 0
+    count := 0
+    i := len(pattern) - 1
+    for offset + len(pattern) < len(file) {
+        if file[i + offset] != pattern[i] {
+            offset += max(1, i - badChar[file[i + offset]])
+            i = len(pattern) - 1
+        } else if i == 0 {
+            result = append(result, offset)
+            count++
+            fmt.Printf("%d found pattern at %d\n", count, offset)
+
+            offset += len(pattern)
+            i = len(pattern) - 1
+        } else {
+            i--
+        }
+    }
+    return result
 }
